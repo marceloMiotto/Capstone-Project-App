66af4e36cddc4521b80c802fd4a46a56f92b6917
Marcelo_Miotto <mmiotto@gmail.com>
2017-03-17 17:23:44 -0300

    feat: Layout Advogado details

--- app/src/main/java/udacitynano/com/br/cafelegal/MainActivity.java
+++ app/src/main/java/udacitynano/com/br/cafelegal/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
+    @SuppressWarnings("deprecation")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
