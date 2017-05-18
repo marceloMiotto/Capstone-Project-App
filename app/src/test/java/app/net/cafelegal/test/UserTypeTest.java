0856f5160061881431e6920a4e081ce9acf385f4
mmiotto <mmiotto@gmail.com>
2017-05-18 20:26:06 -0300

    feat: User Type Unit Tests added

--- app/src/test/java/app/net/cafelegal/test/UserTypeTest.java
+++ app/src/test/java/app/net/cafelegal/test/UserTypeTest.java
@@ -1,30 +1,61 @@
 package app.net.cafelegal.test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import udacitynano.com.br.cafelegal.singleton.UserType;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
-/**
- * Created by Dell on 17/05/2017.
- */
-public class UserTypeTest {
+@RunWith(MockitoJUnitRunner.class)
+public class UserTypeTest   {
+
+    UserType userType;
+    @Mock
+    Context mockContext;
+    @Mock
+    SharedPreferences sharedPreferences;
+
+
+    @Before
+    public void initialize() {
+        when(mockContext.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPreferences);
+    }
+
     @Test
-    public void getInstance() throws Exception {
+    public void getInstanceTest() throws Exception {
+        when(sharedPreferences.getLong(anyString(),anyLong())).thenReturn((long)1);
+        userType = UserType.getInstance(mockContext);
+        assertNotNull(userType);
+    }
+
+    @Test
+    public void isAdvogadoTest() throws Exception {
+        when(sharedPreferences.getString(anyString(),anyString())).thenReturn("ADVOGADO");
+        userType = UserType.getInstance(mockContext);
+        assertTrue(UserType.isAdvogado());
 
     }
 
     @Test
-    public void isAdvogado() throws Exception {
-
+    public void getAppUserTypeTest() throws Exception {
+        when(sharedPreferences.getString(anyString(),anyString())).thenReturn("ADVOGADO");
+        userType = UserType.getInstance(mockContext);
+        assertEquals(UserType.getAppUserType(),"ADVOGADO");
     }
 
     @Test
-    public void getAppUserType() throws Exception {
-
-    }
-
-    @Test
-    public void getUserId() throws Exception {
+    public void getUserIdTest() throws Exception {
+        when(sharedPreferences.getLong(anyString(),anyLong())).thenReturn((long)1);
+        userType = UserType.getInstance(mockContext);
+        assertEquals(UserType.getUserId(),1);
 
     }
 
